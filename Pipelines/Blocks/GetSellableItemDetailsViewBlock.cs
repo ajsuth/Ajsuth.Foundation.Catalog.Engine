c0d8aee8c28a10dadcc61442eb0f3c8cb932c4ab
Andrew Sutherland <andrew.sutherland@live.com>
2019-06-03 07:51:45 +1000

    Issue: #8 Technical Details: - Added Variation Properties to Sellable Item's Variants entity view.

--- Pipelines/Blocks/GetSellableItemDetailsViewBlock.cs
+++ Pipelines/Blocks/GetSellableItemDetailsViewBlock.cs
@@ -12,7 +12,8 @@ namespace Ajsuth.Foundation.Catalog.Engine.Pipelines.Blocks
     using Sitecore.Framework.Conditions;
     using Sitecore.Framework.Pipelines;
     using System;
-    using System.Threading.Tasks;
+	using System.Linq;
+	using System.Threading.Tasks;
 
     /// <summary>
     /// Defines the get sellable item details view pipeline block
@@ -59,9 +60,89 @@ namespace Ajsuth.Foundation.Catalog.Engine.Pipelines.Blocks
                 return await Task.FromResult(entityView).ConfigureAwait(false);
             }
 
-            await this.AddCatalogNavigationView(entityView, sellableItem, context).ConfigureAwait(false);
-            
-            return await Task.FromResult(entityView).ConfigureAwait(false);
+			if (enablementPolicy.CatalogNavigationView)
+			{
+				await this.AddCatalogNavigationView(entityView, sellableItem, context).ConfigureAwait(false);
+			}
+
+			if (entityViewArgument.ViewName.Equals(policy.Master, StringComparison.OrdinalIgnoreCase)
+					&& string.IsNullOrEmpty(entityViewArgument.ForAction))
+			{
+				var isBundle = sellableItem.HasComponent<BundleComponent>();
+				if (isBundle)
+				{
+					return await Task.FromResult(entityView).ConfigureAwait(false);
+				}
+
+				var variantsView = entityView.ChildViews.FirstOrDefault(c => c.Name == policy.SellableItemVariants) as EntityView;
+				UpdateVariantsView(variantsView, sellableItem, context);
+			}
+
+			return await Task.FromResult(entityView).ConfigureAwait(false);
         }
-    }
+		
+		/// <summary>
+		/// Updates variants entity view
+		/// </summary>
+		/// <param name="variantsView">The variants view.</param>
+		/// <param name="sellableItem">The sellable item.</param>
+		/// <param name="context">The context.</param>
+		protected virtual void UpdateVariantsView(EntityView variantsView, SellableItem sellableItem, CommercePipelineExecutionContext context)
+		{
+			var variations = sellableItem.GetComponent<ItemVariationsComponent>().ChildComponents.OfType<ItemVariationComponent>().ToList();
+			if (variations != null && variations.Count <= 0)
+			{
+				return;
+			}
+
+			var variationPropertyPolicy = context.CommerceContext.Environment.GetPolicy<VariationPropertyPolicy>();
+			foreach (var variation in variations)
+			{
+				var variationView = variantsView.ChildViews.FirstOrDefault(c => ((EntityView)c).ItemId == variation.Id) as EntityView;
+				PopulateVariationProperties(variationView, variation, variationPropertyPolicy);
+			}
+		}
+
+		/// <summary>
+		/// Populates the variation properties in the entity view
+		/// </summary>
+		/// <param name="variationView">The variation view.</param>
+		/// <param name="variation">The item variation component.</param>
+		/// <param name="variationPropertyPolicy">The variation property policy.</param>
+		protected virtual void PopulateVariationProperties(EntityView variationView, ItemVariationComponent variation, VariationPropertyPolicy variationPropertyPolicy)
+		{
+			foreach (var variationProperty in variationPropertyPolicy.PropertyNames)
+			{
+				var property = GetVariationProperty(variation, variationProperty);
+
+				var insertIndex = variationView.Properties.Count > 0 ? variationView.Properties.Count - 1 : 0;
+				variationView.Properties.Insert(insertIndex, new ViewProperty
+				{
+					Name = variationProperty,
+					RawValue = property ?? string.Empty,
+					IsReadOnly = true
+				});
+			}
+		}
+
+		/// <summary>
+		/// Gets the variation property
+		/// </summary>
+		/// <param name="variationComponent">The item variation component.</param>
+		/// <param name="variationProperty">The name of the variation property.</param>
+		/// <returns></returns>
+		protected virtual object GetVariationProperty(ItemVariationComponent variationComponent, string variationProperty)
+		{
+			foreach (var component in variationComponent.ChildComponents)
+			{
+				var property = component.GetType().GetProperty(variationProperty);
+				if (property != null)
+				{
+					return property.GetValue(component);
+				}
+			}
+
+			return null;
+		}
+	}
 }
